341fc44f8687117528018f1e0e3a2e81902abe15
lixiaolei19821218 <735534185@qq.com>
2015-04-27 14:00:41 +0800

    handler

---
+++ Handlers/CustomHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Handlers
+{
+    public class CustomHandler : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string time = DateTime.Now.ToShortTimeString();
+            if (context.Request.CurrentExecutionFilePathExtension == ".json")
+            {
+                context.Response.ContentType = "application/json";
+                context.Response.Write(string.Format("{{\"time\": \"{0}\"}}", time));
+            }
+            else
+            {
+                context.Response.ContentType = "text/html";
+                context.Response.Write(string.Format("<span>{0}</span>", time));
+            }
+        }
+    }
+}
---
+++ Handlers/Time.ashx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Handlers
+{
+    /// <summary>
+    /// Time 的摘要说明
+    /// </summary>
+    public class Time : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string time = DateTime.Now.ToShortTimeString();
+            if (IsAjaxRequest(context.Request))
+            {
+                context.Response.ContentType = "application/json";
+                context.Response.Write(string.Format("{{\"time\": \"{0}\"}}", time));
+            }
+            else
+            {
+                context.Response.ContentType = "text/html";
+                context.Response.Write(string.Format("<span>{0}</span>", time));
+            }
+        }
+
+        private bool IsAjaxRequest(HttpRequest request)
+        {
+            return request.Headers["X-Requested-With"] == "XMLHttpRequest" || request["X-Requested-With"] == "XMLHttpRequest";
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
